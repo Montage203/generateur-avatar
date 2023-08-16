269d140d867f34d5d95e113bacc111d9b7ebe8f3
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-16 09:51:11 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -27,13 +27,46 @@ tiles.forEach(tile => {
 })
 
 
-// Ajoutez ces lignes à la fin de votre script2.js
-
 const pseudoInput = document.getElementById("pseudoInput");
 const avatarImg = document.querySelector(".avatar img");
+const actionSelect = document.getElementById("actionSelect");
+const directionSelect = document.getElementById("directionSelect");
+// Ajoutez des sélecteurs pour les autres éléments d'interface utilisateur (head_direction, gesture, size, headonly, etc.)
+
+let action = "std";
+let direction = 2;
+let headDirection = 2;
+let gesture = "std";
+let size = "n";
+let headOnly = 0;
+
+updateAvatar();
+
+function updateAvatar() {
+  const avatarUrl = `https://api.habbocity.me/avatar_image.php?user=${encodeURIComponent(
+    pseudoInput.value
+  )}&action=${action}&direction=${direction}&head_direction=${headDirection}&gesture=${gesture}&size=${size}&headonly=${headOnly}`;
+  avatarImg.src = avatarUrl;
+}
 
 pseudoInput.addEventListener("keyup", () => {
-  const newPseudo = pseudoInput.value;
-  const avatarUrl = `https://api.habbocity.me/avatar_image.php?user=${encodeURIComponent(newPseudo)}&action=std&size=n&head_direction=2&direction=2&gesture=std&headonly=0`;
-  avatarImg.src = avatarUrl;
+  updateAvatar();
+});
+
+// Ajoutez des écouteurs d'événements pour les changements d'options d'avatar (par exemple, boutons pour changer d'action, de direction, etc.)
+actionSelect.addEventListener("change", () => {
+  action = actionSelect.value;
+  updateAvatar();
+});
+
+directionSelect.addEventListener("change", () => {
+  direction = parseInt(directionSelect.value);
+  updateAvatar();
+});
+
+// Ajoutez des écouteurs d'événements pour les autres éléments d'interface utilisateur (head_direction, gesture, size, headonly, etc.)
+// Lorsque ces options changent, appelez simplement la fonction updateAvatar() pour mettre à jour l'avatar en conséquence.
+
+document.getElementById("updateButton").addEventListener("click", () => {
+  updateAvatar();
 });
