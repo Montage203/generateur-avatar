c4873979e91213c879191774644b615886abf879
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-16 08:14:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -56,34 +56,3 @@ $(document).ready(function() {
 	});
 });
 
-
-
-
-
-/** Determine Avatar Position */
-const tiles = document.querySelectorAll(".room .block");
-const avatar = document.querySelector(".avatar");
-const door = document.querySelector(".room .door");
-
-avatarToPos({
-  avatar: avatar,
-  x: door.getBoundingClientRect().left - 30,
-  y: door.getBoundingClientRect().top + 70,
-})
-
-function avatarToPos({avatar, x, y}) {
-  avatar.style.top = y + 30 + "px";
-  avatar.style.left = x + 55 + "px";
-}
-
-tiles.forEach(tile => {
-  tile.addEventListener("click", e => {
-    const tilePos = e.target.getBoundingClientRect();
-    avatarToPos({
-      avatar: avatar,
-      x: tilePos.left,
-      y: tilePos.top
-    });
-  })
-})
-
