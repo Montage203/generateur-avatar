62dc77a262bddf548208b431a684bb5bff3b3391
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-16 08:27:28 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,43 @@
+// Ajoutez ce code en haut de votre script2.js pour définir les déplacements possibles
+const DIRECTIONS = {
+  UP: "ArrowUp",
+  DOWN: "ArrowDown",
+  LEFT: "ArrowLeft",
+  RIGHT: "ArrowRight",
+};
+
+// Mettez à jour la fonction avatarToPos pour déplacer l'avatar en fonction de la direction
+function moveAvatar(direction) {
+  const avatarPos = avatar.getBoundingClientRect();
+  let newX = avatarPos.left;
+  let newY = avatarPos.top;
+
+  if (direction === DIRECTIONS.UP) {
+    newY -= 55; // Déplacer vers le haut
+  } else if (direction === DIRECTIONS.DOWN) {
+    newY += 55; // Déplacer vers le bas
+  } else if (direction === DIRECTIONS.LEFT) {
+    newX -= 55; // Déplacer vers la gauche
+  } else if (direction === DIRECTIONS.RIGHT) {
+    newX += 55; // Déplacer vers la droite
+  }
+
+  avatarToPos({ avatar, x: newX, y: newY });
+}
+
+// Écouteurs d'événements pour les touches directionnelles
+document.addEventListener("keydown", (event) => {
+  if (event.key in DIRECTIONS) {
+    moveAvatar(event.key);
+  }
+});
+
+// Reste du code existant...
+
+
+
+
+
 /** Determine Avatar Position */
 const tiles = document.querySelectorAll(".room .block");
 const avatar = document.querySelector(".avatar");
