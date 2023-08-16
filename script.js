078eee030f855ee480896d92ac672d44805d7394
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-16 08:09:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -55,3 +55,35 @@ $(document).ready(function() {
 		this.blur();
 	});
 });
+
+
+
+
+
+/** Determine Avatar Position */
+const tiles = document.querySelectorAll(".room .block");
+const avatar = document.querySelector(".avatar");
+const door = document.querySelector(".room .door");
+
+avatarToPos({
+  avatar: avatar,
+  x: door.getBoundingClientRect().left - 30,
+  y: door.getBoundingClientRect().top + 70,
+})
+
+function avatarToPos({avatar, x, y}) {
+  avatar.style.top = y + 30 + "px";
+  avatar.style.left = x + 55 + "px";
+}
+
+tiles.forEach(tile => {
+  tile.addEventListener("click", e => {
+    const tilePos = e.target.getBoundingClientRect();
+    avatarToPos({
+      avatar: avatar,
+      x: tilePos.left,
+      y: tilePos.top
+    });
+  })
+})
+
