76c4f8011c10186b5e9e391863cd3db06443dcec
Montage203 <76145046+Montage203@users.noreply.github.com>
2023-08-16 08:33:56 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -27,34 +27,3 @@ tiles.forEach(tile => {
     });
   })
 })
-
-
-
-// Ajoutez ces lignes à la fin de votre script2.js
-
-const arrowButtons = document.querySelectorAll(".arrow-btn");
-
-arrowButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const currentX = parseInt(avatar.style.left, 10);
-    const currentY = parseInt(avatar.style.top, 10);
-
-    let newX = currentX;
-    let newY = currentY;
-
-    if (button.classList.contains("up")) {
-      newY -= 55; // Déplacer vers le haut
-    } else if (button.classList.contains("down")) {
-      newY += 55; // Déplacer vers le bas
-    } else if (button.classList.contains("left")) {
-      newX -= 55; // Déplacer vers la gauche
-    } else if (button.classList.contains("right")) {
-      newX += 55; // Déplacer vers la droite
-    }
-
-    // Vérification pour ne pas sortir de la pièce
-    if (newX >= 0 && newX <= (8 * 55) && newY >= 0 && newY <= (6 * 55)) {
-      avatarToPos({ avatar: avatar, x: newX, y: newY });
-    }
-  });
-});
